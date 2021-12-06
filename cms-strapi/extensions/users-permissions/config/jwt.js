cb4ed238054dfd68ea966df7242780faf438af45
joohoonkim <davidkim2552@gmail.com>
2021-12-05 23:14:13 -0500

    using strapi@v3.3.4

---
+++ cms-strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '934934c0-db14-4104-83e8-a98ffac0368e'
+};
--- strapi-cms/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '05d4635d-507a-4ba3-83b8-2fc79aa38a16'
-};
