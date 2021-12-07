04932deb373b0c6b820bb7a5b65ac8186def569f
joohoonkim <davidkim2552@gmail.com>
2021-12-07 18:59:40 -0500

    changed port number to 1338

--- cms-strapi/config/server.js
+++ cms-strapi/config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 1338),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'a052d7960bc85c654ac46d0c85b9ddf4'),
